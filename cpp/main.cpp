d057b099c9e6fa29eb4166fbebb2884c7add9c68
Ignacio Vizzo <ivizzo@irobot.com>
2017-04-05 18:35:53 -0700

    Initial Commit(Version 6.2.4)

---
+++ cpp/main.cpp
@@ -0,0 +1,28 @@
+#include "clipper.hpp"
+#include <iostream>
+using namespace ClipperLib;
+
+int main()
+{
+	Path subj;
+	Paths solution;
+	subj <<
+	IntPoint(348,257) <<
+	IntPoint(364,148) <<
+	IntPoint(362,148) <<
+	IntPoint(326,241) <<
+	IntPoint(295,219) <<
+	IntPoint(258,88)  <<
+	IntPoint(440,129) <<
+	IntPoint(370,196) <<
+	IntPoint(372,275);
+
+	ClipperOffset co;
+	co.AddPath(subj, jtRound, etClosedPolygon);
+	co.Execute(solution, 2.0);
+
+	std::cout << "solution" << std::endl;
+	std::cout << solution;// << std::endl;
+
+
+}
